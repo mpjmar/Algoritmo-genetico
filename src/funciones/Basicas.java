078e0776e2746c04bec0972a95d8ef6ffd48c65e
m. paz jimenez <meripeich@gmail.com>
2026-01-05 18:24:50 +0100

    delete unnecessary function

--- src/funciones/Basicas.java
+++ src/funciones/Basicas.java
@@ -52,10 +52,4 @@ public class Basicas {
 		}
         return binario;
     }
-
-	public static void swap(int[] array, int pos1, int pos2) {
-		int temp = array[pos1];
-		array[pos1] = array[pos2];
-		array[pos2] = temp;
-	}
 }
